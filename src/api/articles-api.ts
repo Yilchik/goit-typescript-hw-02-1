87f2e9cbadd987a8b0f211ef5024ba078f4d6f47
Yilchik <yulyayulia2@gmail.com>
2024-07-08 21:51:07 +0200

    api

--- src/api/articles-api.ts
+++ src/api/articles-api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-export const getArticlesApi = async (searchQuery, page) => {
+export const getArticlesApi = async (searchQuery: string, page: number) => {
   const { data } = await axios.get("/search/photos", {
     headers: {
       Authorization: `Client-ID D_SPVSOx1r7wQQf7tImOfFzoek9ERDaEib9Ux2P1n7M`,
